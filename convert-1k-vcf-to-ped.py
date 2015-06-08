377bb21ea9c764e56c0c3c8dac5b33d8ec225ed4
Bastian Greshake <bgreshake@googlemail.com>
2015-06-08 08:15:38 +0200

    fixed vcf conversion

--- convert-1k-vcf-to-ped.py
+++ convert-1k-vcf-to-ped.py
@@ -1,17 +1,17 @@
 import glob
 import subprocess
-
-# apparently there's no easy way to do all files for all populations at the same time. this is a crude hack to get at least some interesting populations out for all 22 chromosomes. requires you to have the vcfs in the same folder, along with the sample panel
+import os
 
 vcfs = glob.glob("*.vcf")
 
 pops = "-population FIN -population GBR -population CHS -population CDX -population PUR -population IBS -population ACB -population GWD -population ESN -population MSL -population -CEU -population YRI"
 
 for i in vcfs:
-	print i
-	call = "perl vcf_to_ped_convert.pl -vcf " + i 
+#	print i
+	call = "echo \"perl vcf_to_ped_convert.pl -vcf " + i 
 	call += " -sample integrated_call_samples_v3.20130502.ALL.panel -region "
 	ia = i.split(".")[1].replace("chr","")
-	call += ia + ":1-249240543 " + pops
-	subprocess.call(call,shell=True)
-	
+	call += ia + ":1-249240543 " + pops + "\"|qsub -V -S /bin/bash -cwd -j y -r y -q all.q -N vcf" + ia
+	#subprocess.call(call,shell=True,cwd=os.getcwd())
+	print call
+
